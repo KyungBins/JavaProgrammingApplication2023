0c1db7190e167033b0f04658cf7754a8c38d4cb7
KyungBins <ryosu0706@gmail.com>
2023-09-20 14:58:13 +0900

    week04 v20

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,9 +1,22 @@
 public class PokemonGame {
     public static void main(String[] args) {
-        NoFly noFly = new NoFly();
-        Pikachu pikachu = new Pikachu(noFly); // Association, Has-a관계 (Association) 강한 결속 관계
-//        pikachu.setFlyable(new NoFly()); //프로그램 실행 중 동적으로 하늘을 나는 방식을 변경
-        pikachu.performFly();
-
+        System.out.println("포켓몬 게임을 시작합니다. \n야생 포켓몬이 나타났습니다.");
+//        System.out.println(Math.random()); // 0.0 <= x < 1.0   -- Math.random은 1을 넘지않음
+//        System.out.println((int)(Math.random()*6)+1); // 1.0 <= x <= 6.0
+        
+        //적군 포켓몬스터 랜덤생성
+        int enemyPick = ((int)(Math.random()*3));
+        if (enemyPick == 0){
+            NoFly noFly = new NoFly();
+            Pikachu pikachu = new Pikachu(noFly);
+        } else if(enemyPick == 1){
+            NoFly noFly = new NoFly();
+            Squirtle squirtle = new Squirtle(noFly);
+        } else if(enemyPick == 2){
+            Wings wings = new Wings();
+            Charizard charizard = new Charizard(wings);
+        } else {
+            System.out.println("여기는 영원히 실행 안됩니다.");
+        }
     }
 }
